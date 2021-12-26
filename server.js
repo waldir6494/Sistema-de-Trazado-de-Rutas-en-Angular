6bb1459f3639d89df9dc83f8c78c0fac50cf72a6
waldir <waldir6323@gmail.com>
2021-12-26 15:15:57 -0500

    correccion errores en node server

--- server.js
+++ server.js
@@ -7,9 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/front_proyecto'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/front_proyecto/'}),
-);
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'src', 'index.html');
+  res.sendFile(index);
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
