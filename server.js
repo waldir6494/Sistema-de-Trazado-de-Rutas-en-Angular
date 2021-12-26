e99efa28ecec898725b5c3c15941abb23449c4d6
waldir <waldir6323@gmail.com>
2021-12-25 21:17:47 -0500

    nombre de aplicacion

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/angularapp'));
+app.use(express.static(__dirname + '/argon-dashboard-angular'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+  res.sendFile(path.join(__dirname + '/argon-dashboard-angular/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
