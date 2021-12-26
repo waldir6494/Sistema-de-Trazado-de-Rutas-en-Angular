292744aeafe2bb3e91e10ae36a29adfc335f6dbd
waldir <waldir6323@gmail.com>
2021-12-26 16:35:34 -0500

    correccion errores en node server

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
-/* 
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-
-app.use(express.static('./dist/front_proyecto'));
+const root = path.join(__dirname, 'dist');
+  //express.use static with the directory
+app.use(express.static(root));
 
 app.get('*', function (req, res) {
   const index = path.join(__dirname, 'src', 'index.html');
@@ -13,44 +14,4 @@ app.get('*', function (req, res) {
 });
 
 
-app.listen(process.env.PORT || 8080); */
-
-
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const testimonialsRoutes = require('./routes/testimonials.routes');
-const concertsRoutes = require('./routes/concerts.routes');
-const seatsRoutes = require('./routes/seats.routes');
-const app = express();
-
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
-//STATIC
-  // get directory where is index.html
-const root = path.join(__dirname, 'client', 'build');
-  //express.use static with the directory
-app.use(express.static(root));
-  //express get request any (*) root, please use file that is on root directory configure above.
-
-app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'src', 'index.html');
-    res.sendFile(index);
-});
-
-
-//Dynamic Api
-
-app.use('/api/', testimonialsRoutes);
-app.use('/api/', concertsRoutes);
-app.use('/api/', seatsRoutes);
-
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
-});
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
-});
+app.listen(process.env.PORT || 8080);
