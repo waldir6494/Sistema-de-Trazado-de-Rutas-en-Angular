88f7d6806dedacd4c2aaabff4b4d90fa03931172
waldir <waldir6323@gmail.com>
2021-12-26 14:42:32 -0500

    correccion errores en node server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/argon-dashboard-angular'));
+app.use(express.static('../dist/argon-dashboard-angular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/argon-dashboard-angular/'}),
+    res.sendFile('index.html', {root: '../dist/argon-dashboard-angular/'}),
 );
 
 // Start the app by listening on the default Heroku port
